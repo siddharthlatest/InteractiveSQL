412f98827fb68c639ca8cecc3fb139ecf9b5bd1c
Siddharth Kothari <sids.aquarius@gmail.com>
2012-12-26 16:35:05 +0530

    Generic tester for both RA and SQL queries

--- tester.py
+++ tester.py
@@ -1,26 +1,28 @@
-def lineNum(num):
-    if num > 0:
-        return "- "+str(num)+"."
-    return "-"
-
 def start():
     import sys
-    if len(sys.argv) != 3:
-        print "Usage: ", sys.argv[0], "<file_path for file to test> <file_path for correct query output>"
+    LINE_PADDER = 0
+    if len(sys.argv) not in [3,4] or (len(sys.argv) is 4 and '--RA' not in sys.argv) \
+          or (len(sys.argv) is 3 and '--RA' in sys.argv):
+        print "Usage: ", sys.argv[0], "[--RA] <file_path for file to test> <file_path for solution query>."
+        print "--RA indicates RA mode. The default check is in SQL mode."
         sys.exit()
+    if len(sys.argv) is 4:
+        LINE_PADDER=4
+        sys.argv.remove('--RA')
     with open(sys.argv[2]) as f:
         contentL = f.readlines()
         content = set(contentL)
     with open(sys.argv[1]) as f:
         usercontent = f.readlines()
     if len(usercontent) > 0 and usercontent[0].startswith("Error"):
-        print "".join(usercontent[:-3]),
+        print "- "+"- ".join(usercontent[:-(LINE_PADDER-1)]),
     else:
-        mismatch =  [lineNum(i-1)+" "+usercontent[i] for i in xrange(len(usercontent)-4) if usercontent[i] not in content]
+        mismatch =  [usercontent[i] for i in \
+              xrange(len(usercontent)-LINE_PADDER) if usercontent[i] not in content]
         if len(mismatch) != 0:
-            print "".join(mismatch),
-        missing = ["* "+con for con in contentL[:-4] if con not in usercontent]
+            print "- "+"- ".join(mismatch),
+        missing = [con for con in contentL[:len(content)-LINE_PADDER] if con not in usercontent]
         if len(missing) != 0:
-            print "".join(missing),
+            print "* "+"* ".join(missing),
 
 start()
