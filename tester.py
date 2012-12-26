a89bd844d2ddae8e41dbf139538ade32369a6e04
Siddharth Kothari <sids.aquarius@gmail.com>
2012-12-26 14:42:59 +0530

    Show missing rows in feedback
    Previous feedback only showed incorrect rows, not the missing ones

--- tester.py
+++ tester.py
@@ -1,17 +1,26 @@
+def lineNum(num):
+    if num > 0:
+        return "- "+str(num)+"."
+    return "-"
+
 def start():
     import sys
     if len(sys.argv) != 3:
         print "Usage: ", sys.argv[0], "<file_path for file to test> <file_path for correct query output>"
         sys.exit()
     with open(sys.argv[2]) as f:
-        content = set(f.readlines())
+        contentL = f.readlines()
+        content = set(contentL)
     with open(sys.argv[1]) as f:
         usercontent = f.readlines()
     if len(usercontent) > 0 and usercontent[0].startswith("Error"):
         print "".join(usercontent[:-3]),
     else:
-        mismatch =  [str(i-1)+". "+usercontent[i] for i in xrange(len(usercontent)-4) if usercontent[i] not in content]
+        mismatch =  [lineNum(i-1)+" "+usercontent[i] for i in xrange(len(usercontent)-4) if usercontent[i] not in content]
         if len(mismatch) != 0:
             print "".join(mismatch),
+        missing = ["* "+con for con in contentL[:-4] if con not in usercontent]
+        if len(missing) != 0:
+            print "".join(missing),
 
 start()
